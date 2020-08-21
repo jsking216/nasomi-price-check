ad3e38144cc895e76e7077952c4c1fc409e35ad6
jsking216 <jarf1337@gmail.com>
2020-08-20 23:22:48 -0400

    need to finish string parsing, break main into functions

--- main.go
+++ main.go
@@ -12,30 +12,6 @@ func main() {
 	itemPtr := flag.String("item", "", "ffxi item name")
 	flag.Parse()
 
-	ahPayload := strings.NewReader("itemname=" + strings.ReplaceAll(*itemPtr, " ", "+"))
-	ahRes, err := http.Post(
-		"https://na.nasomi.com/auctionhouse/data/ah-data/searchItemByName.php",
-		"text/html; charset=UTF-8",
-		ahPayload,
-	)
-	if err != nil {
-		fmt.Println(err)
-	}
-	ahData, _ := ioutil.ReadAll(ahRes.Body)
-	ahRes.Body.Close()
-
-	fmt.Println(ahData)
-
-	bazaarUrl := "https://na.nasomi.com/status/bazaar.php"
-	bazaarRes, bazaarErr := http.Get(bazaarUrl)
-	if bazaarErr != nil {
-		fmt.Println(bazaarErr)
-	}
-	bazaarData, _ := ioutil.ReadAll(bazaarRes.Body)
-	bazaarRes.Body.Close()
-	fmt.Println(bazaarData)
-	// use equalFold to find the item if it is in the list
-
 	toCapitalized := strings.Title(*itemPtr)
 	withUnderscores := strings.ReplaceAll(toCapitalized, " ", "_")
 	vendorUrl := "https://classicffxi.fandom.com/wiki/"  + withUnderscores
@@ -44,6 +20,42 @@ func main() {
 		fmt.Println(vendorErr)
 	}
 	vendorData, _ := ioutil.ReadAll(vendorRes.Body)
-	vendorRes.Body.Close()
-	fmt.Println(vendorData)
+	defer vendorRes.Body.Close()
+	vendorString := string(vendorData)
+	priceInfo := vendorString[strings.Index(vendorString, "Price:"):strings.Index(vendorString, "Gil")]
+	itemIDStringMarker := "http://www.edenxi.com/db/items/"
+	itemIDStartIndex := strings.Index(vendorString, itemIDStringMarker) + len(itemIDStringMarker)
+	forParsingItemID := vendorString[itemIDStartIndex:itemIDStartIndex+20]
+	fmt.Println(priceInfo)
+	fmt.Println(forParsingItemID)
+
+
+	ahPayload := strings.NewReader("itemid=17396")
+	ahRes, err := http.Post(
+		"https://na.nasomi.com/auctionhouse/data/ah-data/searchItem.php",
+		"text/html; charset=UTF-8",
+		ahPayload,
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+	ahData, _ := ioutil.ReadAll(ahRes.Body)
+	defer ahRes.Body.Close()
+
+	fmt.Printf("%s\n", ahData)
+
+	bazaarUrl := "https://na.nasomi.com/status/bazaar.php"
+	bazaarRes, bazaarErr := http.Get(bazaarUrl)
+	if bazaarErr != nil {
+		fmt.Println(bazaarErr)
+	}
+	bazaarData, _ := ioutil.ReadAll(bazaarRes.Body)
+	defer bazaarRes.Body.Close()
+	bazaarString := string(bazaarData)
+	dataUpper, subUpper := strings.ToUpper(bazaarString), strings.ToUpper(*itemPtr)
+	if (strings.Contains(dataUpper, subUpper)) {
+		// parse price
+	} else {
+		fmt.Println("No bazaar listings found for: " + string(*itemPtr))
+	}
 }
