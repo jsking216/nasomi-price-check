d83fc9edbf9a21fa048b6524faa3adb1a3359b7a
jsking216 <jarf1337@gmail.com>
2020-09-01 22:11:12 -0400

    print based on error status

--- main.go
+++ main.go
@@ -14,20 +14,26 @@ func main() {
 	itemID, price, vendorErr := parsers.VendorParse(itemPtr)
 	if vendorErr != nil {
 		fmt.Println(vendorErr)
+	} else {
+		fmt.Println("Vendor Price: " + price)
 	}
-	fmt.Println(price)
-	fmt.Println(itemID)
 
 	ahPrice, ahErr := parsers.AuctionParse(itemID)
 	if ahErr != nil {
 		fmt.Println(ahErr)
+	} else {
+		fmt.Println("Auction House Price: " + ahPrice)
 	}
 
 	bazaarPrice, bazaarErr := parsers.BazaarParse(itemPtr)
 	if bazaarErr != nil {
 		fmt.Println(bazaarErr)
+	} else {
+		fmt.Println("Bazaar Price: " + bazaarPrice)
 	}
 
-	fmt.Println(ahPrice)
-	fmt.Println(bazaarPrice)
+	if vendorErr != nil && ahErr != nil && bazaarErr != nil {
+		fmt.Println("ERROR: All price requests failed -- are you sure the item name is correct?")
+	}
+
 }
